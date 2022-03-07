dc9492dcf896f30ef58d5dc9da091e1c0a05fac5
Sam Xu <saxu@microsoft.com>
2022-03-07 13:25:00 -0800

    Init the project

---
+++ src/Microsoft.OData.Query/Services/IQueryNodeBinder.cs
@@ -0,0 +1,15 @@
+﻿//-----------------------------------------------------------------------
+// Copyright (c) .NET Foundation and Contributors. All rights reserved.
+// See License.txt in the project root for license information.
+//-----------------------------------------------------------------------
+
+using Microsoft.OData.Query.Ast;
+using System.Linq.Expressions;
+
+namespace Microsoft.OData.Query.Services
+{
+    public interface IQueryNodeBinder
+    {
+        Expression Bind(QueryNode node, BinderContext context);
+    }
+}
---
+++ test/Microsoft.OData.Query.Tests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Xunit;
+
+namespace Microsoft.OData.Query.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
